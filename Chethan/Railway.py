f0981b759ceee43f06a31030aa541b278f7cee23
Chethan Shetty <chethan.shetty@intimetec.com>
2020-01-09 13:04:03 +0530

    adding Chethan Files

--- Chethan/Railway.py
+++ Chethan/Railway.py
@@ -1,3 +1,9 @@
-Seat=input()
-if int(int(Seat)%6==0):
-    print("Side Upper")
+Seat=input("Enter The seat Number:")
+if int(Seat)%6==0:
+    print("Side Upper Berth (Dark Green)")
+elif (int(Seat)+1)%6==0:
+    print("Side Lower Berth (Light Green)")
+elif (int(Seat)%6!=0 and int(Seat)%2==0):
+    print("Upper Berth (Light Blue)")
+else:
+    print("Lower Berth (Yellow)")
