d041405af520a4022387a69f2e4b91100d953909
Jai Mathur <jai.mathur@intimetec.com>
2020-01-23 13:27:35 +0530

    Adding Bike Problem

---
+++ Jai/Calculator.py
@@ -0,0 +1,17 @@
+#
+def addition(num1,num2):
+    print(num1+num2)
+   
+def substraction(num1,num2):
+    print(num1-num2) 
+
+def multiplication(num1,num2):
+    print(num1*num2)
+
+def division(num1,num2):
+    print(num1/num2)
+
+addition(1,2)
+substraction(1,2)
+multiplication(1,2)
+division(1,2)
