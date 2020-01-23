031d9e91345c1f2a4e1a1a3e17bf434c8677cd86
Chethan Shetty <chethan.shetty@intimetec.com>
2020-01-23 12:54:03 +0530

    Added Bike App

---
+++ Chethan/bike.py
@@ -0,0 +1,25 @@
+class Bike:
+    def __init__(self,Name,speed):
+        self.name=Name
+        self.speed=speed
+    def description(self):
+        return "{} is running in {} speed".format(self.name,self.speed)
+
+class RoyalEnfield(Bike):
+    def speedup(self):
+        self.speed=self.speed+10
+        print("speed is increased")
+        return self.speed
+        #self.gear=self.gear+1
+    def speeddown(self):
+        self.speed=self.speed-10
+        print("speed is decreased")
+        return self.speed
+        #self.gear=self.gear-1
+
+a=RoyalEnfield("Royal Enfield",20)
+b=a.speedup()
+c=a.speedup()
+print(a.description())
+d=a.speeddown()
+print(a.description())
