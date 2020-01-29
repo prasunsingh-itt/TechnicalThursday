01a80274a659c8d437325f3ccaa5131407603051
intime <narasimha.b@intimetec.com>
2020-01-29 10:04:08 +0530

    code refactored

--- Narasimha/bike-app/client.py
+++ Narasimha/bike-app/client.py
@@ -2,29 +2,38 @@ from yamaha import Yamaha
 from kawasaki import Kawasaki
 from person import Person
 
-name = input("Please enter rider name: ")
-person = Person(name)
-print("Hello ", person.getName(), "!!!")
-print("Available bikes for ride are \n 1. Yamaha 2. Kawasaki")
-bikeType = input("Please choose bike for riding: ")
 
-while int(bikeType) != 1 and int(bikeType) != 2:
-    print("You have selected invalid option...!!")
+def selectBike():
+    print("Available bikes for ride are \n 1. Yamaha 2. Kawasaki :")
     bikeType = input("Please choose bike for riding: ")
+    while bikeType != "1" and bikeType != "2":
+        print("Available bikes for ride are \n 1. Yamaha 2. Kawasaki :")
+        print("You have selected invalid option...!!")
+        bikeType = input("Please choose bike for riding: ")
+    bike = None
+    if bikeType == "1":
+        bike = Yamaha(100, 5)
+    else:
+        bike = Kawasaki(150, 6)
+    return bike
 
-bike = None
-if int(bikeType) == 1:
-    bike = Yamaha(100, 5)
-else:
-    bike = Kawasaki(150, 6)
 
-bike.start()
-while bike.getGearPosition() < bike.getNoOfGears():
-    bike.gearUp()
-bike.gearUp()
+def readRiderName():
+    name = input("Please enter rider name: ")
+    person = Person(name)
+    print("Hello ", person.getName(), "!!!")
 
-while bike.getGearPosition() != 0:
-    bike.gearDown()
-bike.gearDown()
 
-bike.stop()
+readRiderName()
+bike = selectBike()
+switcher = {"1": bike.start, "2": bike.gearUp,
+            "3": bike.gearDown, "4": bike.stop}
+option = ''
+while option != None:
+    option = input(
+        """Select operation 1. Start Engine 2. Gear Up  3. Gear Down 4. Stop Engine 5. Exit :""")
+    operation = switcher.get(option, None)
+    if operation is None:
+        option = None
+    else:
+        operation()
