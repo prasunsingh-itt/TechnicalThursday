862872ef193660621bf59545a6f2e3a788fffb92
Likhit Gupta <likhit.gupta@intimetec.com>
2020-03-09 20:54:32 +0530

    Student class

---
+++ Taha/Lbry Mgmt Source Code/Student.py
@@ -0,0 +1,18 @@
+class Student:
+    
+    def __init__(self):
+        super().__init__()
+        self.studentId
+        self.name
+        self.phoneNumber
+        self.email
+        self.noOfBooks
+
+    def Borrow_Book(self):
+        pass
+
+    def Return_Book(self):
+        pass
+
+    def Pay_Fine(self):
+        pass
